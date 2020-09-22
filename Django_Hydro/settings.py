889abc1bbc709b9634af1234277ed7a8656f70fb
Wenbo Shi <wenboshi@mit.edu>
2020-09-21 21:54:04 -0400

    changed host

--- Django_Hydro/settings.py
+++ Django_Hydro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p^80r2xa$*hfe=uwz!$w#61n_iow@cd$z0cmi_1ej=0=gnfx*+'
 DEBUG = True
 
 # ALLOWED_HOSTS = ['django-react-hydro.eba-r2aqsr9e.us-east-1.elasticbeanstalk.com']
-ALLOWED_HOSTS =['54.86.210.152']
+ALLOWED_HOSTS =['*']
 
 # Application definition
 
