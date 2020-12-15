b06c1f0a9aeaa96d0a9f840be0f17fcd87bbf61c
Wenbo Shi <wenboshi@mit.edu>
2020-12-15 11:49:45 -0500

    remove pymysql

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@
 import os
 import sys
 
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Hydro.settings')
